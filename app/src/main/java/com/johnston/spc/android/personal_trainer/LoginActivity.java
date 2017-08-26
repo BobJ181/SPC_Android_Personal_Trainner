37dd907f0119dfe988b1c3184608711d9cf24d45
BobJ181 <johnston@johnstonr.com>
2017-08-26 19:23:27 -0400

    Updated Manifest and string values.

--- app/src/main/java/com/johnston/spc/android/personal_trainer/LoginActivity.java
+++ app/src/main/java/com/johnston/spc/android/personal_trainer/LoginActivity.java
@@ -347,6 +347,7 @@ public class LoginActivity extends AppCompatActivity implements LoaderCallbacks<
 
             if (success) {
                 Intent intent = new Intent(LoginActivity.this, MainActivity.class);
+                startActivity(intent);
             } else {
                 mPasswordView.setError(getString(R.string.error_incorrect_password));
                 mPasswordView.requestFocus();
