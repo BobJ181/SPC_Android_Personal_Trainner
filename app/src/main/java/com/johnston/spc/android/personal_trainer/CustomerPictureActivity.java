cadbe3454061cdd71971ef4b6dc3d766dcfd7e5b
BobJ181 <johnston@johnstonr.com>
2017-09-04 20:05:38 -0400

    Update

---
+++ app/src/main/java/com/johnston/spc/android/personal_trainer/CustomerPictureActivity.java
@@ -0,0 +1,13 @@
+package com.johnston.spc.android.personal_trainer;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+public class CustomerPictureActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_customer_picture);
+    }
+}
